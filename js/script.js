cd9118557bd52efff4a5ff99029faf072dbf4402
web1html <mnm0627@naver.com>
2022-07-26 16:02:06 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+$(".menu>ul>li").hover(
+    function(){
+        $(this).children("ul").stop().slideDown("slow");
+    },function(){
+        $(this).children("ul").stop().slideUp("slow")
+    }
+);
+
+var slide = $(".slide>a>img");
+var sno = 0;
+var eno = slide.length-1;
+
+setInterval("autoslide()",3000);
+
+function autoslide(){
+    $(slide[sno]).animate({
+        opacity : "1"
+    },1000);
+    sno++;
+    if(sno > eno){
+        sno = 0;
+    }
+    $(slide[sno]).animate({
+        opacity : "0"
+    },1000)
+}
+
+function openPop() {
+    $(".pop").show();
+}
+
+function closePop(){
+    $(".pop").hide();
+}
